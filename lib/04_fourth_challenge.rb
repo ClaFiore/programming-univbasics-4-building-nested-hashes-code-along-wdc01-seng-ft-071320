7917d70b30c5a132379a5f75b495ada0d7266432
Claudia Borghini <claborghini@gmail.com>
2020-06-30 14:25:58 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -9,14 +9,14 @@ def fourth_challenge
           :name => "Benvolio",
           :age => "17",
           :attitude => "worried"
-        }, 
+        },
         Mercutio {
           :name => "Mercutio",
           :age => "30",
           :attitude => "confused"
         }
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
@@ -27,6 +27,6 @@ def fourth_challenge
    }
 }
 
-  
+
 
 end
